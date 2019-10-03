cd1862975093209a9f54942b6c84e02f3d29b976
Cláudio Yuri <claudio.y07@gmail.com>
2019-10-03 13:04:33 -0300

    add cors no backend e salvando id do usuario no localstorage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
+const cors = require('cors');
 const app = express();
 
 mongoose.connect('mongodb://localhost/aircnc', { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
