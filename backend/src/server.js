acfef98f42a6b4084218f4548c9fb4760bef44fb
Cláudio Yuri <claudio.y07@gmail.com>
2019-10-05 13:40:52 -0300

    finalizado cadastro usando react

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 mongoose.connect('mongodb://localhost/aircnc', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333, () => {
